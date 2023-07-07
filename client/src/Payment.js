ab461a5c2c8bdd127c4adaccd27650c32bbd4245
Genaro <gjmg1612@gmail.com>
2023-07-07 13:21:27 -0600

    version 1 estable

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -27,7 +27,7 @@ function Payment() {
 
   return (
     <>
-      <h1>React Stripe and the Payment Element</h1>
+      <h1>Paga facilmente con tus tarjetas bancarias o genera tu referencia en oxxo</h1>
       {clientSecret && stripePromise && (
         <Elements stripe={stripePromise} options={{ clientSecret }}>
           <CheckoutForm />
